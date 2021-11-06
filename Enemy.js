b43d85028c972ceeb3993f21fb735d22445927bf
gudb <gbs26@hi.is>
2021-11-06 02:41:05 +0000

    Beinagrind fyrir enemy

---
+++ Enemy.js
@@ -0,0 +1,17 @@
+// ======
+// ENEMY
+// ======
+
+"use strict";
+/* jshint browser: true, devel: true, globalstrict: true */
+
+
+function Enemy(descr) {
+    
+    // Common inherited setup logic from Entity
+    this.setup(descr);
+
+    // Default sprite, if not otherwise specified
+}
+
+Enemy.prototype = new Enemy();
